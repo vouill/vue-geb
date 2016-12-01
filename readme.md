6eb1022f3cc62a5687e7e8edb5a0be9ebcd70cb0
Vouill <vouillon.thibaud@gmail.com>
2016-12-01 11:36:34 +0100

    First commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Vue-geb 
+
+Vue-geb is a vue Global Event Bus plugin to help broadcast event accros the app using the power of observables.
+
+## Install
+
+* npm install --save vue-geb
+
+## Use
+
+In your `main.js` : 
+```
+import geb from 'geb'
+
+Vue.use(geb)
+```
---
+++ vue-geb.js
@@ -0,0 +1,30 @@
+import {Subject} from 'rxjs/Subject'
+
+const vouillMindBus = new Subject()
+vouillMindBus.subscribe(o => {
+    console.log(`[VMB] received : ${JSON.stringify(o)}`)
+})
+
+var geb = {}
+geb.install = function (Vue, options) {
+    Vue.directive('geb-emit', {
+        inserted: function (el, binding) {
+            el.addEventListener('click', (event) => {
+                vouillMindBus.next(binding.value)
+        })
+        }
+    })
+
+    Vue.prototype.$geb = function () {
+        return {
+            getBus: function () {
+                return vouillMindBus
+            },
+            emit: function (obj) {
+                vouillMindBus.next(obj)
+            }
+        }
+    }
+}
+
+module.exports = geb
